9cdc23287132e73b78ca2264affc8bd2ab7017bc
Pei Wang <pxw172@ist.psu.edu>
2015-09-03 11:44:12 -0400

    Implement equals and hashCode for Tuple so that it can be used as Map keys.

--- src/edu/psu/ist/plato/kaiming/util/Tuple.java
+++ src/edu/psu/ist/plato/kaiming/util/Tuple.java
@@ -3,12 +3,29 @@ package edu.psu.ist.plato.kaiming.util;
 public class Tuple<F, S> {
     public F first;
     public S second;
+    
     public Tuple() {
   	    first = null;
   	    second = null;
     }
+    
     public Tuple(F first, S second) {
         this.first = first;
         this.second = second;
+    }
+    
+    @Override
+    public boolean equals(Object e) {
+        if (e instanceof Tuple) {
+            Tuple<?, ?> t = (Tuple<?, ?>)(e);
+            return t.first.equals(first) && t.second.equals(second);
+        }
+        else
+            return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() + second.hashCode();
     }
 }
