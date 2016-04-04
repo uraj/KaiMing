3102cc0a971c40d64a0f1572e634e22ebdfa86d7
Pei Wang <pxw172@ist.psu.edu>
2016-04-04 01:11:06 -0400

    The call statement is a DefStmt as well.

--- src/main/java/edu/psu/ist/plato/kaiming/x86/ir/CallStmt.java
+++ src/main/java/edu/psu/ist/plato/kaiming/x86/ir/CallStmt.java
@@ -1,8 +1,9 @@
 package edu.psu.ist.plato.kaiming.x86.ir;
 
 import edu.psu.ist.plato.kaiming.x86.CallInst;
+import edu.psu.ist.plato.kaiming.x86.Register;
 
-public class CallStmt extends Stmt {
+public class CallStmt extends DefStmt {
 
 	private Expr mTarget;
 	
@@ -13,6 +14,17 @@ public class CallStmt extends Stmt {
     
     public Expr getTarget() {
     	return mTarget;
+    }
+
+    @Override
+    public Lval getLval() {
+        // TODO: Make this value configurable with a "CallingConvention"
+        // structure
+        // FIXME: Theoretically, all caller-save registers can be
+        // updated by a call. For now, we assume the analyzed code 
+        // is "good", meaning the compiler will restore these registers
+        // if they are to be used later in the same routine.
+        return Reg.getReg(Register.getRegister("eax"));
     }
 
 }
