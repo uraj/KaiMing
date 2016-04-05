66ed51462cf6c4644e92f15157487962c78e491b
Pei Wang <pxw172@ist.psu.edu>
2016-04-05 16:37:23 -0400

    More set utils

--- src/main/java/edu/psu/ist/plato/kaiming/util/SetUtil.java
+++ src/main/java/edu/psu/ist/plato/kaiming/util/SetUtil.java
@@ -4,10 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetUtil {
+    
     static public <T> Set<T> union(Set<T> u, Set<T> v) {
-        Set<T> r = new HashSet<T>();
-        r.addAll(u);
+        Set<T> r = new HashSet<T>(u);
         r.addAll(v);
+        return r;
+    }
+    
+    static public <T> Set<T> difference(Set<T> u, Set<T> v) {
+        Set<T> r = new HashSet<T>(u);
+        r.removeAll(v);
+        return r;
+    }
+    
+    static public <T> Set<T> intersection(Set<T> u, Set<T> v) {
+        Set<T> r = new HashSet<T>(u);
+        r.retainAll(v);
         return r;
     }
 }
