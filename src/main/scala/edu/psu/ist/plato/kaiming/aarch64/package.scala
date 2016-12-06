2a7118c60f14c7e534ef7e855aed410c5849f406
Pei Wang <pxw172@ist.psu.edu>
2016-12-06 16:47:00 -0500

    Move short type definitions into package objects

--- src/main/scala/edu/psu/ist/plato/kaiming/aarch64/package.scala
+++ src/main/scala/edu/psu/ist/plato/kaiming/aarch64/package.scala
@@ -6,4 +6,20 @@ package object aarch64 {
   import scala.language.implicitConversions
   implicit def toAArch64Inst(e: MachEntry[AArch64]) = e.asInstanceOf[Instruction]
   
+  sealed abstract class Flag(val name: String, val index: Int) extends MachFlag[AArch64]
+
+  object Flag {
+    case object C extends Flag("C", 0)
+    case object Z extends Flag("Z", 1)
+    case object N extends Flag("N", 2)
+    case object V extends Flag("V", 3)
+  }
+  
+  class Function(override val label: Label, insts: Vector[Instruction])
+      extends MachProcedure[AArch64](insts) {
+  
+    val mach = AArch64Machine
+    
+  }
+  
 }
--- src/main/scala/edu/psu/ist/plato/kaiming/arm/package.scala
+++ src/main/scala/edu/psu/ist/plato/kaiming/arm/package.scala
@@ -8,4 +8,20 @@ package object arm {
   import scala.language.implicitConversions
   implicit def toARMInstruction(e: MachEntry[ARM]) = e.asInstanceOf[Instruction]
   
+  sealed abstract class Flag(val name: String, val index: Int) extends MachFlag[ARM]
+
+  object Flag {
+    case object C extends Flag("C", 0)
+    case object Z extends Flag("Z", 1)
+    case object N extends Flag("N", 2)
+    case object V extends Flag("V", 3)
+  }
+  
+  class Function(override val label: Label, insts: Vector[Instruction])
+      extends MachProcedure[ARM](insts) {
+  
+    val mach = ARMMachine
+  
+  }
+  
 }
